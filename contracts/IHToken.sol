b6f7d459288313af3891ddbbd9c9e38b396da5aa
ihossain-6 <ismumhossain@gmail.com>
2023-02-15 12:58:42 +0600

    Update name

--- contracts/IHToken.sol
+++ contracts/IHToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DappToken is ERC20 {
+contract IHToken is ERC20 {
     constructor() ERC20("Ismum Hossain Token", "IHT"){
         _mint(msg.sender, 1000000000000000000000000);
     }
