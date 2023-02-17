2a7651895348aa8288d9b536f087c337fe801bfc
ihossain-6 <ismumhossain@gmail.com>
2023-02-17 13:00:33 +0600

    Add mint function

--- contracts/test/MockWETH.sol
+++ contracts/test/MockWETH.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockWETH is ERC20 {
-    constructor() ERC20("Mock WETH", "WETH"){}
+    constructor() ERC20("Mock WETH", "WETH"){
+        _mint(msg.sender, 1000000000000000000000);
+    }
 }
