c7998c680aca6d77ac9a5a8c59e87cc8aaa42262
ihossain-6 <ismumhossain@gmail.com>
2023-02-18 12:55:28 +0600

    Update more interactions

--- scripts/stake_and_unstake.py
+++ scripts/stake_and_unstake.py
@@ -1,9 +1,16 @@
-from brownie import DeFi
+from brownie import DeFi, MockWETH
+from scripts.set_token_and_price import main
+from scripts.helpful_scripts import get_account
 
 def main():
+    main()
+    non_owner = get_account(1)
     defi = DeFi[-1]
     amount = 10
-    id = 1
+    id = 0
+    weth = MockWETH[-1]
+    weth.transfer(non_owner, 20)
+    weth.approve(defi.address, 15)
     tokenTx = defi.stake(amount, id)
     tokenTx.wait(1)
     print(f'Staked {amount} token...')
