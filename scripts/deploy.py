95612768f65411dd9410388762b0bc3f7122b686
ihossain-6 <ismumhossain@gmail.com>
2023-04-02 13:54:11 +0600

    Remove unnecessary variable

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,5 @@
 from scripts.helpful_scripts import get_account
 from brownie import IHToken, DeFi, network, config
-from web3 import Web3
-
-KEPT_BALANCE = Web3.toWei(100, "ether")
 
 def deploy_defi_and_ih_token():
     account = get_account()
